2e4d186191fdbbb42e5b515b8895177ce003c7f8
Baynar <stynkata@gmail.com>
2020-08-06 12:25:24 +0300

    add copying hex string to clipboard

--- packet_analyzer/ui/views/object_tree.py
+++ packet_analyzer/ui/views/object_tree.py
@@ -5,6 +5,10 @@ from PySide2.QtWidgets import QTreeView, QAction, QActionGroup, QMenu, QFileDial
 
 import os
 
+try:
+    from Tkinter import Tk
+except ImportError:
+    from tkinter import Tk
 
 class ObjectTree(QTreeView):
 
@@ -21,6 +25,9 @@ class ObjectTree(QTreeView):
 
         self.action_export_hex = QAction("Export as hex string", self)
         self.menu.addAction(self.action_export_hex)
+
+        self.action_copy_hex = QAction("Copy as hex string", self)
+        self.menu.addAction(self.action_copy_hex)
 
     def setModel(self, model: PySide2.QtCore.QAbstractItemModel):
         super(ObjectTree, self).setModel(model)
@@ -39,10 +46,17 @@ class ObjectTree(QTreeView):
                     file = open(file_path, "wb")
                     file.write(node.buffer)
                     file.close()
-                else:
+                if action == self.action_export_hex:
                     file_path, _ = QFileDialog.getSaveFileName(self, "Save file")
                     file = open(file_path, "w")
                     file.write(node.buffer.hex())
                     file.close()
+                if action == self.action_copy_hex:
+                    r = Tk()
+                    r.withdraw()
+                    r.clipboard_clear()
+                    r.clipboard_append(node.buffer.hex())
+                    r.update() # now it stays on the clipboard after the window is closed
+                    r.destroy()
         else:
             super(ObjectTree, self).mouseReleaseEvent(event)
